cfbe9e8a5ff01d0422086a0f0218cea6a8571076
ShreeyansB <sbahadkar@rocketmail.com>
2021-11-04 16:46:34 +0530

    :lipstick: Added Arrow SVG
    Added Arrow SVG with animations and fixed few SkillGrid SVGs

---
+++ src/components/SVG/arrow.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useColorMode } from "@chakra-ui/react";
+import { colors } from "./../../Theme";
+import classes from "./arrow.module.css";
+import ScrollAnimation from "react-animate-on-scroll";
+
+function ArrowSVG(props) {
+  const { colorMode } = useColorMode();
+  const myStyle = {
+    fill: "none",
+    stroke: colorMode === "light" ? colors.secondary : colors.alternate,
+  };
+
+  return (
+    <ScrollAnimation animateIn={classes.line}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        x="0"
+        y="0"
+        version="1.1"
+        viewBox="0 0 430 100"
+        xmlSpace="preserve"
+        style={myStyle}
+        className={classes.arrow}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          strokeWidth="9.828"
+          d="M7.32 39.189c34.611 0 34.611 21.622 69.224 21.622 34.611 0 34.611-21.622 69.222-21.622 34.613 0 34.613 21.622 69.226 21.622 34.614 0 34.614-21.622 69.227-21.622 34.616 0 34.616 21.622 69.231 21.622s34.615-21.622 69.23-21.622"
+          // className={classes.line}
+        ></path>
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          strokeWidth="9.828"
+          d="M385.9027 10.2218L422.6808 39 402.9027 83.7782"
+          // className={classes.tip}
+        ></path>
+      </svg>
+    </ScrollAnimation>
+  );
+}
+
+export default ArrowSVG;
--- src/components/UI/SkillBanner.js
+++ src/components/UI/SkillBanner.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { Flex, Text, Heading, Box, useColorMode } from "@chakra-ui/react";
+import {
+  Flex,
+  Text,
+  Heading,
+  Box,
+  useColorMode,
+  Stack,
+} from "@chakra-ui/react";
 import SkillGrid from "./SkillGrid";
 import FadeUpAnim from "./../Animations/FadeUpAnim";
 import { colors } from "./../../Theme";
+import ArrowSVG from "../SVG/arrow";
 
 const SkillBanner = (props) => {
   const { colorMode } = useColorMode();
@@ -16,23 +24,32 @@ const SkillBanner = (props) => {
       mt="30vh"
     >
       <Box flexShrink={0} pe={10}>
-        <FadeUpAnim index={0} delay={1.3}>
-          <Text fontSize="2xl" fontWeight="500" ps="2px">
-            Here are few
-          </Text>
-        </FadeUpAnim>
-        <FadeUpAnim index={1} delay={1.3}>
-          <Heading size="2xl" color={nameColor}>
-            Technologies
-          </Heading>
-        </FadeUpAnim>
-        <FadeUpAnim index={2} delay={1.3}>
-          <Text fontSize="2xl" fontWeight="500" ps="2px" mt="7px">
-            which I've been working with.
-          </Text>
-        </FadeUpAnim>
+        <Stack direction={{base: "row", lg: "column"}} align="flex-start">
+          <Box>
+            <FadeUpAnim index={0} delay={1.3}>
+              <Text fontSize="2xl" fontWeight="500" ps="2px">
+                Here are few
+              </Text>
+            </FadeUpAnim>
+            <FadeUpAnim index={1} delay={1.3}>
+              <Heading size="2xl" color={nameColor}>
+                Technologies
+              </Heading>
+            </FadeUpAnim>
+            <FadeUpAnim index={2} delay={1.3}>
+              <Text fontSize="2xl" fontWeight="500" ps="2px" mt="7px">
+                which I've been working with.
+              </Text>
+            </FadeUpAnim>
+          </Box>
+          <FadeUpAnim index={3} delay={1.3}>
+            <Box w={{base: "3rem", lg:"22rem"}} mt={4} >
+              <ArrowSVG />
+            </Box>
+          </FadeUpAnim>
+        </Stack>
       </Box>
-      <FadeUpAnim index={3} delay={1.3}>
+      <FadeUpAnim index={4} delay={1.3}>
         <SkillGrid />
       </FadeUpAnim>
     </Flex>
